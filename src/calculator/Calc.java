1bf1fd81740d91b5742d434776ac56cf609f84e2
양채호/ICTO55-2 <msyluv@gmail.com>
2015-04-29 17:28:53 +0900

    commit!!

--- src/calculator/Calc.java
+++ src/calculator/Calc.java
@@ -1,5 +1,28 @@
 package calculator;
 
 public class Calc {
-
+	public static void main(String[] args) {
+		
+		int a, b, c;
+		char op = 0;
+		a=10;
+		b=5;
+		
+		switch(op){
+			case'+':
+				c=a+b;
+				break;
+			case'-':
+				c=a-b;
+				break;
+			case'*':
+				c=a*b;
+				break;
+			case'/':
+				c=a/b;
+				break;
+		}
+		
+		
+	}
 }
